f75ad12ecf8da427dfb04a73455ee938848b8852
shenzhaonan <shenzhaonan@fotoable.com>
2018-06-25 16:07:11 +0800

    add menu tools

---
+++ Assets/MenuTools/FolderMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace  Szn.Editor.Menu
+{
+    public class FolderMenu
+    {
+        #region Open folder
+        [MenuItem("Tools/Open Files/PersistentData Folder", priority = 50)]
+        static void OpenPersistentData()
+        {
+            System.Diagnostics.Process.Start(Application.persistentDataPath);
+        }
+
+        [MenuItem("Tools/Open Files/Assets Folder", priority = 53)]
+        static void OpenAssets()
+        {
+            System.Diagnostics.Process.Start(Application.dataPath);
+        }
+
+        [MenuItem("Tools/Open Files/StreamingAssets Folder", priority = 55)]
+        static void OpenStreamingAssets()
+        {
+            System.Diagnostics.Process.Start(Application.streamingAssetsPath);
+        }
+        #endregion
+    }
+}
